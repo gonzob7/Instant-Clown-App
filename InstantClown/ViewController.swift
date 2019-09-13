1558e008d09df57f80bcc70f4ac7372fbbd10b4a
Gonzalo Birrueta <silentcr7@gmail.com>
2019-09-13 08:05:17 -0700

    initial commit

--- InstantClown/ViewController.swift
+++ InstantClown/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clownBtn: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    
 
 }
 
