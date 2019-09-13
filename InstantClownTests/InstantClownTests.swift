0779e0a954fb75a17c7f1c9fe89f819a3901d29f
Gonzalo Birrueta <silentcr7@gmail.com>
2019-09-12 17:09:23 -0700

    Initial Commit

---
+++ InstantClownTests/InstantClownTests.swift
@@ -0,0 +1,34 @@
+//
+//  InstantClownTests.swift
+//  InstantClownTests
+//
+//  Created by Gonzalo Birrueta on 9/12/19.
+//  Copyright © 2019 Gonzalo Birrueta. All rights reserved.
+//
+
+import XCTest
+@testable import InstantClown
+
+class InstantClownTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
