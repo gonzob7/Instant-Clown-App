b2ce81e7deeb2d7bc0eb376f92143ddd3f4736c7
Gonzalo Birrueta <silentcr7@gmail.com>
2019-09-13 08:29:19 -0700

    [add] downloaded clownHorn.mp3

--- InstantClown/ViewController.swift
+++ InstantClown/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var clownBtn: UIButton!
 
+    var audioPlayer = AVAudioPlayer()
+    let clownSound = URL(fileURLWithPath: Bundle.main.path(forResource: "clownHorn", ofType: "mp3")!)
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
